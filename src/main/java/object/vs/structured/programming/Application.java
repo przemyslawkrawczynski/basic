48a53a2d3d7b9ecba17d4452d6476fe3af4a4850
Michal <m.bachmacz@gmail.com>
2023-02-21 18:00:58 +0100

    MB-002 1.1.1

---
+++ src/main/java/object/vs/structured/programming/Application.java
@@ -0,0 +1,27 @@
+package object.vs.structured.programming;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        String name = "Michal";
+        int age = 31;
+        double height = 151;
+
+        UserInfo userinfo = new UserInfo();
+
+        boolean isNameNotNull = userinfo.checkUserName(name);
+        boolean isAgeOver30 = userinfo.checkUserAge(age);
+        boolean isHeightOver160 = userinfo.checkUserHeight(height);
+
+        if (isNameNotNull) {
+            String textAgeHeightOfUser = isAgeOver30 && isHeightOver160 ? "User is older than 30 and higher then 160cm" : "User is younger than 30 or lower than 160cm";
+            System.out.println(textAgeHeightOfUser);
+        } else {
+            System.out.println("isempty");
+        }
+    }
+}
+//            System.out.println("User is older than 30 and higher then 160cm");
+//            System.out.println("User is younger than 30 or lower than 160cm");
+
--- src/main/java/pl/javabasic/module1/objectprogramming/ObjectProgrammingApplication.java
+++
@@ -1,4 +0,0 @@
-package pl.javabasic.module1.objectprogramming;
-
-public class ObjectProgrammingApplication {
-}
