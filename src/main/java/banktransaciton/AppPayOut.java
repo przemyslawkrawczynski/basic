131621e7dd2a5b7392360bfd43de2093d0a0f5c2
Michal <m.bachmacz@gmail.com>
2023-02-24 12:12:35 +0100

    MB 005 1.4.1.

---
+++ src/main/java/banktransaciton/AppPayOut.java
@@ -0,0 +1,10 @@
+package banktransaciton;
+
+public class AppPayOut {
+    public static void main(String[] args) {
+
+        CashMachinePayOut newPayOut = new CashMachinePayOut();
+
+                newPayOut.initiatePayOut();
+    }
+}
--- src/main/java/interfaces2/Knight.java
+++ src/main/java/interfaces2/Knight.java
@@ -4,18 +4,18 @@ public class Knight {
     private Quest quest; //pole klasy o typie interfejsu, do tego pola przypisana zostanie wartość
                          // w trakcie tworzenia nowego obiektu Knight
 
-    public Knight(Quest quest){ //konstruktor, w którego argumencie znajduje się
-                                // interfejs, który jest typem danych, tak samo jak int., itd
+    public Knight(Quest quest) { //konstruktor, w którego argumencie znajduje się
+                                 // interfejs, który jest typem danych, tak samo jak int., itd
         this.quest = quest;
     }
-    public void doQuest(){
-        this.quest.process();  // to pole wywołuje metodę interfejsu -  process()
-                                // nie wiem co się odpierdala w tym wierszu - skąd nagle w metodzie jest this?
 
+    public void doQuest() {
+        this.quest.process();  // to pole wywołuje metodę interfejsu -  process()
+                               // nie wiem co się odpierdala w tym wierszu - skąd nagle w metodzie jest this?
 
     }
 
 }
 
-    // o jednej ważnej rzeczy chuje tu nie piszą, czyi że można tworzyć nowy obiekt klasy
-    // ale używając interfejsu i że można this. stosować też w metodach - do tej pory było tylko w konstruktorach klas
+// o jednej ważnej rzeczy chuje tu nie piszą, czyi że można tworzyć nowy obiekt klasy
+// ale używając interfejsu i że można this. stosować też w metodach - do tej pory było tylko w konstruktorach klas
